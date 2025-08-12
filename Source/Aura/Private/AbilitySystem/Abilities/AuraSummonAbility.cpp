bd9eb585c85af16e078e7ab7ab693ea1e118d04a
noahirving515 <135392068+noahirving515@users.noreply.github.com>
2025-08-12 12:38:53 -0700

    Update AuraSummonAbility.cpp
    - removed all of the debugging from the function
    - added a line trace to the location so that it the location returned will always be on the ground. this will work if there is a slope, stairs, or uneven terrain in the world

--- Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
@@ -3,7 +3,6 @@
 
 #include "AbilitySystem/Abilities/AuraSummonAbility.h"
 
-#include "Kismet/KismetSystemLibrary.h"
 
 TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
 {
@@ -16,13 +15,15 @@ TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
 	for (int32 i = 0; i < NumMinions; i++)
 	{
 		const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
-		const FVector ChosenSpawnLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
+		FVector ChosenSpawnLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
+
+		FHitResult Hit;
+		GetWorld()->LineTraceSingleByChannel(Hit, ChosenSpawnLocation + FVector(0, 0, 400), ChosenSpawnLocation - FVector(0, 0, 400), ECC_Visibility);
+		if (Hit.bBlockingHit)
+		{
+			ChosenSpawnLocation = Hit.ImpactPoint;
+		}
 		SpawnLocations.Add(ChosenSpawnLocation);
-		DrawDebugSphere(GetWorld(), ChosenSpawnLocation, 18, 12, FColor::Green, false, 3.f);
-		
-		UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), Location, Location + Direction * MaxSpawnDistance, 4, FLinearColor::Green, 3.f);
-		DrawDebugSphere(GetWorld(), Location + Direction * MinSpawnDistance, 5, 12, FColor::Red, false, 3.f);
-		DrawDebugSphere(GetWorld(), Location + Direction * MaxSpawnDistance, 5, 12, FColor::Red, false, 3.f);
 	}
 
 	return SpawnLocations;
