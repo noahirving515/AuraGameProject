4c03e33677541f60ba3e549b035ce4e096e42c5b
noahirving515 <135392068+noahirving515@users.noreply.github.com>
2025-08-12 12:21:06 -0700

    Summon Spell.cpp
    Added a new class for summon spell to be assigned to the shaman
    - created a function which loops through number of minions to spawn and sets the min and max distance then chooses a random point for each minion to spawn. The function then returns an array of vectors for spawn locations.
    - this function is blueprint callable and will be used in GA_SummonAbility to spawn minions at the locations when the summon ability is activated

--- Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -8,6 +8,7 @@
 
 class AAuraProjectile;
 class UGameplayEffect;
+struct FGameplayTag;
 /**
  * 
  */
@@ -25,7 +26,7 @@ protected:
 	TSubclassOf<AAuraProjectile> ProjectileClass;
 
 	UFUNCTION(BlueprintCallable, Category="Projectile")
-	void SpawnProjectile(const FVector& ProjectileTargetLocation);
+	void SpawnProjectile(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag);
 
 	
 };
