38933f72d1860b4645f69ca9780601a303264c1c
noahirving515 <135392068+noahirving515@users.noreply.github.com>
2025-08-11 21:12:05 -0700

    Update AuraProjectile.cpp
    Minor Logic update. Had to set bHit to true in a couple of spots so that the impact effect and impact sound werent playing multiple times. When these effects are spawned that bool is now set to true

--- Source/Aura/Private/Actors/AuraProjectile.cpp
+++ Source/Aura/Private/Actors/AuraProjectile.cpp
@@ -52,6 +52,7 @@ void AAuraProjectile::Destroyed()
 		UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
 		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
 		if (LoopingSoundComponent) LoopingSoundComponent->Stop();
+		bHit = true;
 	}
 	Super::Destroyed();
 	
@@ -72,7 +73,8 @@ void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, 
 	{
 		UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
     	UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
-    	if (LoopingSoundComponent) LoopingSoundComponent->Stop();	
+    	if (LoopingSoundComponent) LoopingSoundComponent->Stop();
+		bHit = true;
 	}
 	
 	if (HasAuthority())
