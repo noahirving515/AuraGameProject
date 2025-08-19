68dde16a6dde719d7b13e060b4b883e6ebeea1dc
noahirving515 <135392068+noahirving515@users.noreply.github.com>
2025-08-19 08:37:39 -0700

    Overlay widget controller.cpp updates
    created a new lambda function which binds the on level changed delegate and broadcasts the new level value

--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -32,6 +32,7 @@ struct FUIWidgetRow : public FTableRowBase
 struct FOnAttributeChangeData;
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAttributeChangedSignature, float, NewValue);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPlayerStatChangedSignature, int32, NewValue);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMessageWidgetRowSignature, FUIWidgetRow, Row);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAbilityInfoSignature, const FAuraAbilityInfo&,	Info);
 
@@ -70,6 +71,9 @@ class AURA_API UOverlayWidgetController : public UAuraWidgetController
 
 	UPROPERTY(BlueprintAssignable, Category = "GAS| XP")
 	FOnAttributeChangedSignature OnXPPercentChangedDelegate;
+
+	UPROPERTY(BlueprintAssignable, Category = "GAS| Level")
+	FOnAttributeChangedSignature OnPlayerLevelChangedDelegate;
 
 protected:
 
