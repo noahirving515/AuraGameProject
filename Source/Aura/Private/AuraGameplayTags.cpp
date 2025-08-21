6f62b1e2e9ad4a08f62582b59aaa03264039c256
noahirving515 <135392068+noahirving515@users.noreply.github.com>
2025-08-20 23:29:10 -0700

    Gameplay Tags update
    added two gameplay tags for passive spells one and two so that players will be able to assign their position within the player hud

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -117,6 +117,14 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 		FName("InputTag.4"),
 		FString("Input Tag for 4 Key")
 		);
+	GameplayTags.InputTag_Passive_1 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.Passive.1"),
+		FString("Input Tag for Passive Ability 1")
+		);
+	GameplayTags.InputTag_Passive_2 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.Passive.2"),
+		FString("Input Tag for Passive Ability 2")
+		);
 
 	//Damage types
 	GameplayTags.Damage = UGameplayTagsManager::Get().AddNativeGameplayTag(
