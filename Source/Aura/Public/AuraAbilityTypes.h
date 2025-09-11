f67a0a5534d4369232cf0932b595031b0fc85625
noahirving515 <135392068+noahirving515@users.noreply.github.com>
2025-09-11 12:47:32 -0700

    Update AuraAbilityTypes.h
    - Added 5 new member variables for successful debuff, debuff damage, debuff frequency, debuff duration, and damage types
    - created 5 new getter functions for each of the new member variables
    - and 4 setter functions for the debuff paramaters

--- Source/Aura/Public/AuraAbilityTypes.h
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -53,9 +53,18 @@ struct FAuraGameplayEffectContext : public FGameplayEffectContext
 	public:
 	bool IsCriticalHit() const { return bIsCriticalHit; }
 	bool IsBlockedHit() const { return bIsBlockedHit; }
+	bool IsSuccessfulDebuff() const { return bIsSuccessfulDebuff; }
+	float GetDebuffDamage() const { return DebuffDamage; }
+	float GetDebuffFrequency() const { return DebuffFrequency; }
+	float GetDebuffDuration() const { return DebuffDuration; }
+	TSharedPtr<FGameplayTag> GetDamageType() const { return DamageType; }
 
 	void SetIsCriticalHit(bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
 	void SetIsBlockedHit(bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
+	void SetIsSuccessfulDebuff (bool bInIsDebuff) { bIsSuccessfulDebuff = bInIsDebuff; }
+	void SetDebuffDamage (float InDamage) { DebuffDamage = InDamage; }
+	void SetDebuffFrequency (float InFrequency) { DebuffFrequency = InFrequency; }
+	void SetDebuffDuration (float InDuration) { DebuffDuration = InDuration; }
 
 	/** Custom serialization, subclasses must override this */
 	virtual UScriptStruct* GetScriptStruct() const
@@ -84,6 +93,20 @@ struct FAuraGameplayEffectContext : public FGameplayEffectContext
 	
 	UPROPERTY()
 	bool bIsCriticalHit = false;
+
+	UPROPERTY()
+	bool bIsSuccessfulDebuff = false;
+
+	UPROPERTY()
+	float DebuffDamage = 0.f;
+
+	UPROPERTY()
+	float DebuffFrequency = 0.f;
+
+	UPROPERTY()
+	float DebuffDuration = 0.f;
+
+	TSharedPtr<FGameplayTag> DamageType;
 };
 
 template<>
